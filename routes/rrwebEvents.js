41102e6f47e3768fd0041234cc7219c6cba33be7
skds <1597333865@qq.com>
2023-09-12 17:48:29 +0800

    update

--- routes/rrwebEvents.js
+++ routes/rrwebEvents.js
@@ -27,8 +27,8 @@ async function decodeErrorStack(stack) {
       column: parseInt(column, 10),
     })
     if (originalPosition.source) {
+      // 获取真实的文件地址
       const source = originalPosition.source.replace(/(\W+)\/(.+)/g, (match, $1, $2) => {
-        // console.log($1, $2)
         return $2
       })
       return `${name} (${source}:${originalPosition.line}:${originalPosition.column})`
@@ -41,18 +41,21 @@ async function decodeErrorStack(stack) {
   return parsedStack
 }
 
-/* GET home page. */
+// TODO 通过不同的项目id进行 error信息存储
 router.post('/save', async function (req, res, next) {
   const error = req.body
   const project = await RrwebProject.findOne({ projectName: error.projectName })
   error.projectId = project.projectId
+  // 还原成源文件的error位置
   const parsedStack = await decodeErrorStack(error.errorInfo.stack)
   error.errorInfo.stack = parsedStack
   RrwebEvents.insertMany(error)
+  // 发送钉钉通知
+  // TODO 限制发送频率 同一个errorstack只发送一次，不同的stack同一个钉钉通知，1分钟只能发送19次，
   axios.post('https://oapi.dingtalk.com/robot/send?access_token=a6b6ca5fb0456352000d565b90b4d23c7871b0df92a37136751d8933f6de98d1', {
     msgtype: 'text',
     text: {
-      content: '监控报警: ' + error.errorInfo.message,
+      content: '监控报警: ' + error.errorInfo.stack,
     },
   })
   res.send({ ok: true })
@@ -75,13 +78,5 @@ router.post('/getEventsFromErrors', async function (req, res, next) {
   const value = results[results.length - 1]
   res.send(value)
 })
-
-// router.post('/get', function(req, res, next) {
-//   RrwebEvents.find({}).then((values)=>{
-//     console.log(values)
-//     res.send(values)
-//   })
-
-// });
 
 module.exports = router
